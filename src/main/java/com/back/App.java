174ee9d0e8016ba7e4551549b971fdaf40bd110f
dpwl0974 <dpwl0974@gmail.com>
2025-08-08 14:33:53 +0900

    feat: red, greem, blue 단계 거치기

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -2,10 +2,6 @@ package com.back;
 
 public class App {
     public int plus(int num1, int num2) {
-        if(num1 == 1 && num2 == 2) {
-            return 3;
-        }
-
-        return 30;
+        return num1 + num2;
     }
 }
