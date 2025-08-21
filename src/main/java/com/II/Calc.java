2fa6bcc2fa3477c119d22f7cf5483998ab67f3fb
dpwl0974 <dpwl0974@gmail.com>
2025-08-21 10:14:43 +0900

    feat: 더하기 기능 구현 green

--- src/main/java/com/II/Calc.java
+++ src/main/java/com/II/Calc.java
@@ -1,8 +1,21 @@
 package com.II;
 
 public class Calc {
-
     public static int run(String s) {
+        String expr = s.replace(" ",""); //1+1
+
+        for(int i = 0; i < expr.length(); i++) {
+            char c = expr.charAt(i);
+
+            if(c == '+' && i > 0) {
+                String left = expr.substring(0, i);
+                String right = expr.substring(i+1);
+                int leftNum = Integer.parseInt(left);
+                int rightNum = Integer.parseInt(right);
+
+                return leftNum + rightNum;
+            }
+        }
         return 0;
     }
 }
